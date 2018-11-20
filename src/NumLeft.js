5d499fe19bb643627d45b78459184528a8bc53f4
Unknown <dr.mihail@yandex.ru>
2018-11-20 21:44:24 +0500

    подсчет количества чисел слева

--- src/NumLeft.js
+++ src/NumLeft.js
@@ -19,37 +19,115 @@ export default class NumTop extends Component {
     render() {
         const {context} = this.props,
                 state = context.state;
+
+                // var str ='z,x,c'
+                // var arr = str.split(',');
+                // // var bbb = row.split('0');
+                // console.log( arr );
+
+        // const aaa = ;
+        // const aaa = state.images['img' + state.selected].arr.map(function(item, i) {
+        //     // var arr = str.split(',');
+        //     console.log( item.split('0') );
+        //     return (
+        //         <div>
+        //             {item}
+        //         </div>
+        //     )
+        // });
+
+        let a = 0;
+
+        // const str = state.images['img' + state.selected].arr.map(function(item, i) {
+        //     item.map(function(val, i) {
+        //         // console.log( val )
+        //         if ( val ) {
+        //             // console.log( val );
+        //             a = val;
+        //         }
+        //         // return a
+        //     })
+        //     // console.log( a )
+        //     // console.log( item )
+        //     // return a
+        //     return a
+        //     // var res = item[i];
+        // });
+
+        // var str = '1010001';
         return (
             <div className='num num_left'>
-                {
-                    state.images['img' + state.selected].numLeft.map(function(row, i) {
-                        return (
-                            <div className='num__col' key={i}>
-                                {
-                                    row.map(function(value, j) {
-                                        return (
-                                            <div className='num__value' key={j}>
-                                                {value}
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
-                        )
+            {
+                state.images['img' + state.selected].arr.map(function(item, i) {
+                    item.map(function(val, i) {
+                        // console.log( val )
+                        if ( val ) {
+                            // console.log( val );
+                            a = val;
+                        }
+                        // return a
                     })
-                }
+                    console.log( a )
+                    // console.log( item )
+                    // return a
+                    return a
+                    // var res = item[i];
+                })
+            }
             </div>
         )
     }
-    // componentDidMount() {
-    //     const {context} = this.props,
-    //             state = context.state;
-    //     state.pic1.numTop.map(function(row, i) {
-    //         console.log(row);
-    //     });
-    // }
+    /*
+    componentDidMount() {
+        const {context} = this.props,
+                state = context.state;
+        let a = 0;
+        const str = state.images['img' + state.selected].arr.map(function(item, i) {
+            item.map(function(val, i) {
+                // console.log( val )
+                if ( val ) {
+                    console.log( val );
+                    a = val;
+                }
+                return a
+            })
+            console.log( a )
+            return item
+            // var res = item[i];
+        });
+    }
+    */
 }
 
 // {row.map(function(cell, j){
 //     return <td className={cell?'true':'false'} key={j}> {cell} </td>;
 // })}
+
+
+    // {
+    //     state.temp.map(function(item, i) {
+    //         return (
+    //             <div>
+    //                 {item}
+    //             </div>
+    //         )
+    //     })
+    // }
+
+// {
+//     state.images['img' + state.selected].numLeft.map(function(row, i) {
+//         return (
+//             <div className='num__col' key={i}>
+//                 {
+//                     row.map(function(value, j) {
+//                         return (
+//                             <div className='num__value' key={j}>
+//                                 {value}
+//                             </div>
+//                         )
+//                     })
+//                 }
+//             </div>
+//         )
+//     })
+// }
