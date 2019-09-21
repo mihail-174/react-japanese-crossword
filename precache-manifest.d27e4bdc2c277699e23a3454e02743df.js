5a1169e681c807cb3803f8d043ea38f533067d5d
Михаил <dr.mihail@yandex.ru>
2019-09-21 17:24:17 +0500

    Updates

---
+++ precache-manifest.d27e4bdc2c277699e23a3454e02743df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dcd6ec7528441832602",
+    "url": "/react-japanese-crossword/static/css/main.07011549.chunk.css"
+  },
+  {
+    "revision": "2dcd6ec7528441832602",
+    "url": "/react-japanese-crossword/static/js/main.2dcd6ec7.chunk.js"
+  },
+  {
+    "revision": "e48d0e5f8bae3c843e8f",
+    "url": "/react-japanese-crossword/static/js/1.e48d0e5f.chunk.js"
+  },
+  {
+    "revision": "89cb581b6a37d9e07c70",
+    "url": "/react-japanese-crossword/static/js/runtime~main.89cb581b.js"
+  },
+  {
+    "revision": "541d99c28f51364405d0aa7b9aef3680",
+    "url": "/react-japanese-crossword/index.html"
+  }
+];
